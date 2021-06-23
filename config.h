a6f62065877ef8adbd3afc80029c8713d9b6f9b2
Vilhelm Bergsøe <vilhelmbergsoe@gmail.com>
2021-06-23 14:42:38 +0200

    updated config.h

--- config.h
+++ config.h
@@ -66,8 +66,9 @@ static const struct arg args[] = {
 	//{ datetime, "%s",           "%F %T" },
 	//{ wifi_perc, "W: (%3s%% on ", "wlp8s0" },
     //{ netspeed_rx, "%sB/s  ", "enp0s3" },
-	{ run_command, ": %4s | ", "amixer sget Master | awk -F\"[][]\" '/%/ { print $2 }' | head -n1" },
-	{ cpu_perc, "[CPU  %s%%]   ", NULL	      },
-	{ ram_perc, "[RAM  %s%%]   ", NULL	      },
+	{ run_command, "[VOL %4s]   ", "amixer sget Master | awk -F\"[][]\" '/%/ { print $2 }' | head -n1" },
+	{ cpu_perc, "[CPU %s%%]   ", NULL	      },
+	{ ram_perc, "[RAM %s%%]   ", NULL	      },
+    { battery_perc, "[BAT %s%%]   ", "BAT0"     },
 	{ datetime, "%s",           "%a %b %d %r" },
 };
